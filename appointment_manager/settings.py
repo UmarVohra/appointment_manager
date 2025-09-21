0ba6312d4a072e4ac12f7c21b558ac5e83e3bfb5
UmarVohra <uvilyas987@gmail.com>
2025-09-21 22:35:52 +0530

    Email system changed

--- appointment_manager/settings.py
+++ appointment_manager/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,9 +138,17 @@ LOGIN_URL = '/admin/login'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'uvilyas987@gmail.com'
+# EMAIL_HOST_PASSWORD = 'dtujizhhimszoqfy'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'uvilyas987@gmail.com'
-EMAIL_HOST_PASSWORD = 'dtujizhhimszoqfy'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
